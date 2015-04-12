5b9c9deaab2828f8bd29eecbe00f35af898b0717
yandech1 <yandecheng94@163.com>
2015-04-12 22:48:29 +0200

    print out all the arguments of main function.

--- myecho.c
+++ myecho.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[], char* envp[]) 
+int main(int argc, char* argv[], char* envp[])
 {
-  printf("DIes ist eine tolle Lösung\n");
+  printf("Die Anzahl der Argumente sind:\t%d\n",argc);
+  int k;
+  for (k = 0; k<argc; k++)
+  {
+    printf("Das %d -te Argument ist :\t%s\n",k,argv[k]);
+  }
 
   return 0;
 }
